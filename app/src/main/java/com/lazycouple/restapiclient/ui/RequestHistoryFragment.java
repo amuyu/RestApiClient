fab54ed605725758ea9cc57e91243370eb8adea6
amuyu <hwajoong.song@gmail.com>
2017-06-12 14:09:39 +0900

    Convert RequestHistoryFragment

--- app/src/main/java/com/lazycouple/restapiclient/ui/RequestHistoryFragment.java
+++ app/src/main/java/com/lazycouple/restapiclient/ui/RequestHistoryFragment.java
@@ -5,14 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.amuyu.logger.Logger;
 import com.lazycouple.restapiclient.MainActivity;
-import com.lazycouple.restapiclient.R;
+import com.lazycouple.restapiclient.databinding.HistoryMainBinding;
 import com.lazycouple.restapiclient.ui.adapter.ReqHistoryAdapter;
 import com.lazycouple.restapiclient.ui.component.DaggerRequestHistoryComponent;
 import com.lazycouple.restapiclient.ui.contract.RequestHistoryContract;
@@ -24,9 +23,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 /**
  * Created by noco on 2016-10-13.
  */
@@ -35,9 +31,8 @@ public class RequestHistoryFragment extends Fragment implements RequestHistoryCo
 
     @Inject RequestHistoryPresenter requestHistoryPresenter;
 
-    @BindView(R.id.sr_history_refresh) SwipeRefreshLayout sr_history_refresh;
-    @BindView(R.id.rv_history_list) RecyclerView rv_history_list;
 
+    private HistoryMainBinding binding;
     private ReqHistoryAdapter historyAdapter;
 
     public static Fragment newInstance() {
@@ -63,10 +58,10 @@ public class RequestHistoryFragment extends Fragment implements RequestHistoryCo
             String historyName = ((ReqHistoryAdapter)adapter).getItem(position);
             ((MainActivity)getActivity()).loadHistoryFragment(historyName);
         }));
-        rv_history_list.setLayoutManager(new LinearLayoutManager(getActivity()));
-        rv_history_list.setAdapter(historyAdapter);
+        binding.rvHistoryList.setLayoutManager(new LinearLayoutManager(getActivity()));
+        binding.rvHistoryList.setAdapter(historyAdapter);
 
-        sr_history_refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        binding.srHistoryRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 // refresh event
@@ -81,9 +76,8 @@ public class RequestHistoryFragment extends Fragment implements RequestHistoryCo
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.history_main, container, false);
-        ButterKnife.bind(this,rootView);
-        return rootView;
+        binding = HistoryMainBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
 
