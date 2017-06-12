eea9bf432128aa0470a82939b77e471904653980
amuyu <hwajoong.song@gmail.com>
2017-06-12 14:20:00 +0900

    Convert ReqHistoryAdapter

--- app/src/main/java/com/lazycouple/restapiclient/ui/adapter/ReqHistoryAdapter.java
+++ app/src/main/java/com/lazycouple/restapiclient/ui/adapter/ReqHistoryAdapter.java
@@ -1,22 +1,18 @@
 package com.lazycouple.restapiclient.ui.adapter;
 
 import android.content.Context;
+import android.databinding.ViewDataBinding;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.lazycouple.restapiclient.R;
+import com.android.databinding.library.baseAdapters.BR;
+import com.lazycouple.restapiclient.databinding.ReqHistoryRowBinding;
 import com.lazycouple.restapiclient.ui.listener.RecyclerItemClickListener;
+import com.lazycouple.restapiclient.util.BindingHolder;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 /**
  * Created by noco on 2016-10-13.
@@ -46,18 +42,16 @@ public class ReqHistoryAdapter extends RecyclerView.Adapter<ReqHistoryAdapter.Hi
 
     @Override
     public HistoryHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        View v = LayoutInflater.from(mContext).inflate(R.layout.req_history_row, parent, false);
-        HistoryHolder historyHolder = new HistoryHolder(v);
-        ButterKnife.bind(historyHolder, v);
-        return historyHolder;
+        ReqHistoryRowBinding binding = ReqHistoryRowBinding.inflate(LayoutInflater.from(mContext), parent, false);
+        return new HistoryHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(HistoryHolder holder, int position) {
         String str = getItem(position);
-        holder.tv_req_history_row.setText(str);
-        holder.ll_history.setOnClickListener(v-> {
+        holder.bind(str);
+        ReqHistoryRowBinding binding = (ReqHistoryRowBinding)holder.getBinding();
+        binding.llHistory.setOnClickListener(v->{
             if(recyclerItemClickListener != null)
                 recyclerItemClickListener.onItemClick(ReqHistoryAdapter.this, position);
         });
@@ -72,16 +66,16 @@ public class ReqHistoryAdapter extends RecyclerView.Adapter<ReqHistoryAdapter.Hi
         return histories.size();
     }
 
-    static class HistoryHolder extends RecyclerView.ViewHolder {
+    public class HistoryHolder extends BindingHolder<String> {
 
-        @BindView(R.id.tv_req_history_row)
-        TextView tv_req_history_row;
-        
-        @BindView(R.id.ll_history)
-        LinearLayout ll_history;
+        public HistoryHolder(ViewDataBinding binding) {
+            super(binding);
+        }
 
-        public HistoryHolder(View itemView) {
-            super(itemView);
+
+        @Override
+        public void bind(String variable) {
+            binding.setVariable(BR.title, variable);
         }
     }
 }
